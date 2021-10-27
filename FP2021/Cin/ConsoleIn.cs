dfdb203dd13b5d51d87cdfa02c663329a75955e4
Goldkraw <raulalex311@live.com>
2021-10-27 11:14:09 +0300

    Return 0 instead of throwing exception.

--- FP2021/Cin/ConsoleIn.cs
+++ FP2021/Cin/ConsoleIn.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace Cin
 {
-    public static class Cin
+    public static class ConsoleIn
     {
         /// <summary>
         /// Citeste un grup de caractere din consola.
@@ -54,7 +54,7 @@ namespace Cin
         /// <summary>
         /// Citeste urmatorul int dat ca si input in consola.
         /// </summary>
-        /// <returns>valoarea int data.</returns>
+        /// <returns>valoarea int data sau 0 daca este intalnita o exceptie.</returns>
         /// <exception cref="FormatException">daca valoarea data nu este formata doar din cifre 0-9.</exception>
         /// <exception cref="OverflowException">daca valoarea data este mai mare de <see cref="int.MaxValue"/>.</exception>
         public static int NextInt()
@@ -63,17 +63,19 @@ namespace Cin
 
             try
             {
-                value = int.Parse(NextToken());
+                value =  int.Parse(NextToken());
             }
             catch (FormatException e)
             {
                 Console.WriteLine("Valoarea data nu poate fi transformata in int!");
-                throw;
+                value = 0;
+                //throw;
             }
             catch (OverflowException e)
             {
-                Console.WriteLine($"Valoarea data este mai mare decat {int.MaxValue}!");
-                throw;
+                Console.WriteLine($"Valoarea data este mai mare decat {int.MaxValue} sau mai mica decat {int.MinValue}!");
+                value = 0;
+                //throw;
             }
 
             return value;
@@ -82,7 +84,7 @@ namespace Cin
         /// <summary>
         /// Citeste urmatorul double dat ca si input in consola.
         /// </summary>
-        /// <returns>valoarea double data.</returns>
+        /// <returns>valoarea double data sau 0 daca este intalnita o exceptie.</returns>
         /// <exception cref="FormatException">daca valoarea data nu este alcatuita doar din cifre 0-9 si caracterele . si ,</exception>
         /// <exception cref="OverflowException">daca valoarea data este mai mare de <see cref="Double.MaxValue"/></exception>
         public static double NextDouble()
@@ -96,12 +98,44 @@ namespace Cin
             catch (FormatException e)
             {
                 Console.WriteLine("Valoarea data nu poate fi transformata in double!");
-                throw;
+                value = 0;
+                //throw;
             }
             catch (OverflowException e)
             {
-                Console.WriteLine($"Valoarea data este mai mare decat {double.MaxValue}!");
-                throw;
+                Console.WriteLine($"Valoarea data este mai mare decat {double.MaxValue} sau mai mica decat {double.MinValue}!");
+                value = 0;
+                //throw;
+            }
+
+            return value;
+        }
+        
+        /// <summary>
+        /// Citeste urmatorul long dat ca si input in consola.
+        /// </summary>
+        /// <returns>valoarea long data sau 0 daca este intalnita o exceptie.</returns>
+        /// <exception cref="FormatException">daca valoarea data nu este alcatuita doar din cifre 0-9</exception>
+        /// <exception cref="OverflowException">daca valoarea data este mai mare decat <see cref="long.MaxValue"/> sau mai mica decat <see cref="long.MinValue"/>.</exception>
+        public static long NextLong()
+        {
+            long value;
+
+            try
+            {
+                value = long.Parse(NextToken());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Valoarea data nu poate fi transformata in long!");
+                value = 0;
+                //throw;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"Valoarea data este mai mare decat {long.MaxValue} sau mai mica decat {long.MinValue}!");
+                value = 0;
+                //throw;
             }
 
             return value;
