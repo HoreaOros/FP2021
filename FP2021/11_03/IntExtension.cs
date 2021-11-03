778dd592e3a26f58c80725b76904637cb1a7d48f
Horea Oros <horea.oros@gmail.com>
2021-11-03 11:33:03 +0200

    add 11_03 project - stars, coins, unit testing, suma cifre, palindrom

---
+++ FP2021/11_03/IntExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_03
+{
+    public static class IntExtension
+    {
+        public static bool IsPalindrome(this int n)
+        {
+            return n == n.Reverse();
+        }
+        public static int Reverse(this int n)
+        {
+            int result = 0;
+            int cifra;
+            while (n > 0)
+            {
+                cifra = n % 10;
+                n = n / 10;
+
+                result = result * 10 + cifra;
+            }
+
+            return result;
+        }
+    }
+}
