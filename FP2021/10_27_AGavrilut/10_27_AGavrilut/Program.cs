6fa8b873c1bf52005fa16cad3f9e700d098816da
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2021-11-08 23:30:29 +0200

    Rezolvat ToDo 10_27

--- FP2021/10_27_AGavrilut/10_27_AGavrilut/Program.cs
+++ FP2021/10_27_AGavrilut/10_27_AGavrilut/Program.cs
@@ -1,15 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _10_27_AGavrilut
+namespace _10_27
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int n;
+            Console.WriteLine("Introduceti un numar pozitiv");
+            n = int.Parse(Console.ReadLine());
+            LengthMaxCollatz(n);
+           
+
+            int lengthCollatz, a, b, maxI = 0, max = 0;
+            Console.WriteLine("Introduceti capetele de interval:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+                lengthCollatz = LengthCollatz(i);
+                if (lengthCollatz > max)
+                {
+                    max = lengthCollatz;
+                    maxI = i;
+                }
+            }
+            Console.WriteLine($"Cea mai lunga secventa este pentru numarul {maxI}");
+
+        }
+
+        public static void LengthMaxCollatz(int n)
+        {
+            int nr = 0;
+            int max = 1;
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+                Console.Write($"{n} ");
+                nr++;
+                if (n > max)
+                {
+                    max = n;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Lungimea secventei este {nr}");
+            Console.WriteLine($"Cel mai mare numar din secventa este {max}");
+        }
+
+        public static int LengthCollatz(int n)
+        {
+            int nr = 0;
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+                nr++;
+            }
+            return nr;
         }
     }
 }
